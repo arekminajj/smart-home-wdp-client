5379a3dc3f26b10fbf1116ed8ac416aa577cfc33
Arek Cios <arek123113@gmail.com>
2023-10-18 19:46:20 +0200

    Add controller.py

---
+++ controller.py
@@ -0,0 +1,33 @@
+import requests
+
+BASE_URL = 'https://wdp-smart-home.azurewebsites.net'
+
+class HomeStatus:
+    def __init__(self, API_KEY):
+        self.API_KEY = API_KEY
+
+    def get(self):
+        res = requests.get(BASE_URL)
+        data = res.json()
+
+        return data
+    
+    def update_light(self, light_1):
+        res = requests.post(BASE_URL + '/light', json={
+        "light_1": light_1,
+        "API_KEY": self.API_KEY
+        })
+
+        data = res.json()
+
+        return data
+    
+    def update_temp_setting(self, temp_set):
+        res = requests.post(BASE_URL + '/temp-set', json={
+        "temp_set": temp_set,
+        "API_KEY": self.API_KEY
+        })
+        
+        data = res.json()
+
+        return data
